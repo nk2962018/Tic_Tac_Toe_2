01f09ec636ae60abcfed823917182bb71e3ae92f
Nisha <nkolivia1234@gmail.com>
2023-07-12 00:46:12 +0530

    Refactor:destructure constants for better code readability.

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -5,25 +5,35 @@ import { Constants } from "./constants/Constants";
 
 const TicTacToe = () => {
 
-  const [tiles, setTiles] = useState(Array(Constants.NO_OF_TILES_IN_THE_BOARD).fill(Constants.EMPTY));
+  const {
+    TIC,
+    TAC,
+    TOE,
+    NO_OF_TILES_IN_THE_BOARD,
+    EMPTY,
+    PLAYER_X,
+    PLAYER_O
+  } = Constants;
+
+  const [tiles, setTiles] = useState(Array(NO_OF_TILES_IN_THE_BOARD).fill(EMPTY));
   const [currentPlayer, setCurrentPlayer] = useState(Constants.PLAYER_X);
 
   const updatingTileOnClicking = (position) => {
     const updatedTiles = [...tiles];
     if(updatedTiles[position]) return;
-    updatedTiles[position] = currentPlayer === Constants.PLAYER_X ? Constants.PLAYER_X : Constants.PLAYER_O;
+    updatedTiles[position] = currentPlayer === PLAYER_X ? PLAYER_X : PLAYER_O;
     setTiles(updatedTiles);
     nextPlayerTurn();
   };
 
   const nextPlayerTurn = () => {
-    setCurrentPlayer(currentPlayer === Constants.PLAYER_X ? Constants.PLAYER_O : Constants.PLAYER_X );
+    setCurrentPlayer(currentPlayer === PLAYER_X ? PLAYER_O : PLAYER_X );
   }
 
   return (
   <div className="tictactoe-container" data-testid="tictactoe">
     <div className="title" data-testid="title">
-      {Constants.TIC} <span className="tac-color">{Constants.TAC}</span> {Constants.TOE} 
+      {TIC} <span className="tac-color">{TAC}</span> {TOE} 
     </div>
     <Board tiles={tiles} onClick={updatingTileOnClicking}/>
   </div>
