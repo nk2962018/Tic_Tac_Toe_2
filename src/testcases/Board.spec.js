b7ee41181b9aa532a07d03ac23b3b3b411c85a9d
Nisha <nkolivia1234@gmail.com>
2023-07-11 21:48:49 +0530

    Refactor:group similar tasks in testcase file.

--- src/testcases/Board.spec.js
+++ src/testcases/Board.spec.js
@@ -3,16 +3,18 @@ import Board from "../components/Board";
 import { TestConstants } from "../components/constants/TestConstants";
 
 describe(("Board works fine when"), () => {
+    
+    let tiles;
+    beforeEach(() => {    
+        render(<Board tiles={Array(TestConstants.NO_OF_TILES_IN_THE_BOARD).fill(TestConstants.EMPTY)}/>)
+        tiles = screen.getAllByTestId("tile");
+    });
 
     it(("render 3X3 tiles"), () => {
-        render(<Board tiles={Array(TestConstants.NO_OF_TILES_IN_THE_BOARD).fill(TestConstants.EMPTY)}/>)
-        const tiles = screen.getAllByTestId("tile");
         expect(tiles).toHaveLength(TestConstants.NO_OF_TILES_IN_THE_BOARD);
     });
 
     it(("renders 3X3 board with no initial values"), () =>{
-        render(<Board tiles={Array(TestConstants.NO_OF_TILES_IN_THE_BOARD).fill(TestConstants.EMPTY)}/>)
-        const tiles= screen.getAllByTestId("tile");
         tiles.forEach((tile) => {
             expect(tile.textContent).toBe(TestConstants.EMPTY);
         });
